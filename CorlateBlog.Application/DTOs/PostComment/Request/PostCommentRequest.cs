04bc0f4fe044d4784eec4f27b912ea912890c863
ayomide000 <akandegbolahanayomide@gmail.com>
2025-10-23 14:37:37 +0100

    postcomment files added

---
+++ CorlateBlog.Application/DTOs/PostComment/Request/PostCommentRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorlateBlog.Application.DTOs.PostComment.Request
+{
+    internal class PostCommentRequest
+    {
+    }
+}
---
+++ CorlateBlog.Application/Services/PostComment/IPostCommentService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorlateBlog.Application.Services.PostComment
+{
+    internal interface IPostCommentService
+    {
+    }
+}
