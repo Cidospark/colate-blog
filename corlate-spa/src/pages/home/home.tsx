89fade410da6ed842f9e0bef5aeab82651e2593f
Cidospark <cidospark27@gmail.com>
2025-10-24 14:07:05 +0100

    added spa-project layout

--- corlate-spa/src/pages/home/home.tsx
+++ corlate-spa/src/pages/home/home.tsx
@@ -1,1 +1,16 @@
-export default function 
+import "./home.css"
+
+function Home(){
+    return <div className="my-container">
+        <div className="title">
+            <h1>Blogs</h1>
+            <div>A simple blog built by devs in training.</div>
+        </div>
+        <div className="main-area">
+            <div className="left-side">Left side</div>
+            <div className="right-side">Right side</div>
+        </div>
+    </div>
+}
+
+export default Home;
