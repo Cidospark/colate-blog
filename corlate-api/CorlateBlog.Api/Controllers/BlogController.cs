f2312207c43ec3122bebf30d5592d3bf43555b4b
CEN-Smart <echinnannachukwuemeka@gmail.com>
2025-10-25 09:24:35 +0100

    set up tailwindcss

---
+++ corlate-api/CorlateBlog.Api/Controllers/BlogController.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CorlateBlog.Application.DTOs;
+using CorlateBlog.Application.DTOs.BlogDTO.Request;
+using CorlateBlog.Application.Services;
+using CorlateBlog.Application.Services.PostBlogServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorlateBlog.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BlogController : ControllerBase
+    {
+
+        private readonly ILogger<BlogController> _logger;
+        private readonly IBlogService _blogService;
+
+        public BlogController(ILogger<BlogController> logger, IBlogService blogService)
+        {
+            _logger = logger;
+            _blogService = blogService;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] BlogRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _blogService.AddBlogAsync(request);
+            return CreatedAtAction(nameof(GetSingle), new { id = result.Data?.Id }, result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSingle([FromRoute] string id)
+        {
+            var result = await _blogService.GetSingleBlogAsync(id);
+            if (result.StatusCode == 200)
+            {
+                return Ok(result);
+            }
+            return NotFound(result);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Get([FromQuery] int page, [FromQuery] int size)
+        {
+            var result = await _blogService.GetAllBlogsAsync(page, size);
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update([FromRoute] string id, [FromBody] BlogRequest request)
+        {
+            var result = await _blogService.UpdateBlogAsync(id, request);
+            if (result.StatusCode == 200)
+            {
+                return Ok(result);
+            }
+
+            return NotFound(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] string id)
+        {
+            var result = await _blogService.DeleteBlogAsync(id);
+            if (result.StatusCode == 200)
+            {
+                return Ok(result);
+            }
+
+            return NotFound(result);
+        }
+
+    }
+}
--- corlate-spa/src/pages/home/home.tsx
+++ corlate-spa/src/pages/home/home.tsx
@@ -1,16 +1,18 @@
-import "./home.css"
+import './home.css';
 
-function Home(){
-    return <div className="my-container">
-        <div className="title">
-            <h1>Blogs</h1>
-            <div>A simple blog built by devs in training.</div>
-        </div>
-        <div className="main-area">
-            <div className="left-side">Left side</div>
-            <div className="right-side">Right side</div>
-        </div>
-    </div>
+function Home() {
+	return (
+		<div className='my-container'>
+			<div className='title'>
+				<h1>Blogs</h1>
+				<div>A simple blog built by devs in training.</div>
+			</div>
+			<div className='main-area'>
+				<div className='left-side bg-rose-400'>Left side</div>
+				<div className='right-side'>Right side</div>
+			</div>
+		</div>
+	);
 }
 
 export default Home;
