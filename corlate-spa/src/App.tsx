b3c554b6ee6ed4fef7dd9c2aaedfd3382d1a6750
Joyx001 <mmaobiefuna@gmail.com>
2025-10-25 16:30:24 +0100

    Added Gallery UI: component, pages, service

--- corlate-spa/src/App.tsx
+++ corlate-spa/src/App.tsx
@@ -1,10 +1,17 @@
-
-import './App.css'
-import Home from './pages/home/home'
+import "./App.css";
+import Home from "./pages/home/home";
+import GalleryPage from "./pages/gallery/galleryPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-
-  return <Home />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/gallery" element={<GalleryPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- corlate-spa/src/pages/home/home.tsx
+++ corlate-spa/src/pages/home/home.tsx
@@ -1,16 +1,22 @@
-import "./home.css"
+import "./home.css";
+import Gallery from "../../components/gallery/gallery";
 
-function Home(){
-    return <div className="my-container">
-        <div className="title">
-            <h1>Blogs</h1>
-            <div>A simple blog built by devs in training.</div>
+function Home() {
+  return (
+    <div className="my-container">
+      <div className="title">
+        <h1>Blogs</h1>
+        <div>A simple blog built by devs in training.</div>
+      </div>
+      <div className="main-area">
+        <div className="left-side">Left side</div>
+        <div className="right-side">
+          <p>Right side</p>
+          <Gallery />
         </div>
-        <div className="main-area">
-            <div className="left-side">Left side</div>
-            <div className="right-side">Right side</div>
-        </div>
+      </div>
     </div>
+  );
 }
 
 export default Home;
