ba8f5e16a35cc213d0259faa8d5f03546956c828
Joyx001 <mmaobiefuna@gmail.com>
2025-10-25 04:26:40 +0100

    Added Gallery: response, repo interface and class, services interface and class, controller

---
+++ corlate-api/CorlateBlog.Api/Controllers/GalleryController.cs
@@ -0,0 +1,46 @@
+﻿using CorlateBlog.Application.Services.Gallery;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorlateBlog.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GalleryController : ControllerBase
+    {
+        private readonly IGalleryService _galleryService;
+
+        public GalleryController(IGalleryService galleryService) 
+        {
+            _galleryService = galleryService;
+        }
+
+        [HttpGet("featured-photos")]
+        public async Task<IActionResult> GetFeaturedPhotosAsync()
+        {
+            var response = await _galleryService.GetAllPhotosAsync(page: 1, size: 6);
+            return StatusCode(response.StatusCode, response);
+        }
+
+
+        [HttpGet("all-photos")]
+        public async Task<IActionResult> GetAllPhotosAsync([FromQuery] int page = 1, [FromQuery] int size = 12)
+        {
+            var response = await _galleryService.GetAllPhotosAsync(page, size);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        //[HttpGet]
+        //public async Task<IActionResult> GetPhotos([FromQuery] int page = 0, [FromQuery] int size = 0)
+        //{
+        //    if (page <= 0 || size <= 0)
+        //    {
+        //        var response = await _galleryService.GetAllPhotosAsync(1, 6);
+        //        return StatusCode(response.StatusCode, response);
+        //    }
+
+        //    var pagedResponse = await _galleryService.GetAllPhotosAsync(page, size);
+        //    return StatusCode(pagedResponse.StatusCode, pagedResponse);
+        //}
+    }
+}
